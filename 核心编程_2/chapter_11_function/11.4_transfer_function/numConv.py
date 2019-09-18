52cf4eb1ef707049e03adfc3a1b32d097a61519a
root <1962610298@qq.com>
2019-09-19 02:05:25 +0800

    传递函数

---
+++ 核心编程_2/chapter_11_function/11.4_transfer_function/numConv.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+# -*- coding=UTF-8 -*-
+
+def convert(func, seq):
+    """conv.sequence of numbers to same type"""
+    return [func(eachNum) for eachNum in seq]
+
+myseq = (123, 45.67, -6.2e8, 999999999)
+print(convert(int, myseq))
+print(convert(float, myseq))
