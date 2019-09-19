75ea5392697a79ffbcc0940914eed99181398604
root <1962610298@qq.com>
2019-09-20 02:01:17 +0800

    默认参数

---
+++ 核心编程_2/chapter_11_function/11.5_formal_arguments/11.5.2_default_arguments/grabWeb.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python3
+# -*- coding=UTF-8 -*-
+
+import urllib.request
+import sys
+
+def catch(url="http://www.baidu.com"):
+    """爬取网页"""
+    try:
+        response = urllib.request.urlopen(url)
+    except Exception as e:
+        print("Error:{}".format(e))
+    html = response.read()
+    html_str = bytes.decode(html, 'utf-8')
+    lines = html_str.readlines()
+    return lines
+
+def firstNotBlank(lines):
+    """返回首个不为空的字符串"""
+    for startLine in lines:
+        if not startLine.strip():
+            continue # 终止当前循环，并忽略剩余的语句
+    else:
+        return sartLineLine
+
+def endNotBlank(lines):
+    """返回首个末尾不为空的字符串"""
+    lines.reverse()
+    endLine = firstNotBlank(lines)
+    return endLine
+
+def main():
+    """模块入口函数"""
+    url = input("Enter the url: ").strip()
+    if url:
+        html_file = catch(url)
+    else:
+        html_file = catch()
+    first_line = firstNotBlank(html_file)
+    end_line = endNotBlank(html_file)
+    print("first_line:{}\nend_line:{}".format(first_line, end_line))
+
+if __name__ == "__main__":
+    main()
