010394ef4b50837f87cda21c623b3545855b7d01
root <1962610298@qq.com>
2019-10-13 12:45:18 +0800

    update xcount

--- 核心编程_2/chapter_14_execute_env/14.4_execute_object_declare_and_buildin_function/14.4.4_exec/xcount.py
+++ 核心编程_2/chapter_14_execute_env/14.4_execute_object_declare_and_buildin_function/14.4.4_exec/xcount.py
@@ -2,7 +2,15 @@
 # -*- coding:UTF-8 -*-
 
 x = 0
+expr = """
 print("x is currently:", x)
 while x < 5:
     x += 1
     print("incrementing x to:", x)
+"""
+def func():
+    exec(expr)
+    exec(expr, {'x': 3})
+
+if __name__ == "__main__":
+    func()
