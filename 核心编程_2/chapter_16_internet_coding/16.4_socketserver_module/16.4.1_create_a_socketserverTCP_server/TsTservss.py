2f4f416681d3f5604874d61080624d8abb0c4c0f
root <1962610298@qq.com>
2019-10-23 17:17:51 +0800

    创建一个socketserverTCP服务器

---
+++ 核心编程_2/chapter_16_internet_coding/16.4_socketserver_module/16.4.1_create_a_socketserverTCP_server/TsTservss.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# -*- coding:UTF-8 -*-
+
+from SocketServer import (TCPServer as TCP, StreamRequestHandler as SRH)
+from time import ctime
+
+HOST = '0.0.0.0'
+PORT = 21567
+ADDR = (HOST, PORT)
+
+class MyRequestHandler(SRH):
+    def handle(self):
+        print"...connected from: ", self.client_address
+        self.wfile.write("[%s] %s" % (ctime(), self.rfile.readline()))
+
+tcpServ = TCP(ADDR, MyRequestHandler)
+print "waiting for connection..."
+tcpServ.serve_forever()
