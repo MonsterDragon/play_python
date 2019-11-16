99ae182dd0cb3eede763da4e4c7d435248bce58e
root <1962610298@qq.com>
2019-11-16 18:54:16 +0800

    文本分词

---
+++ cookbook/chapter_2_string_and_file/2.18_text_segmentation/token.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding:UTF-8 -*-
+
+import re
+from collections import namedtuple
+
+NAME = r'(?P<NAME>[a-zA_Z_][a-zA-Z_0-9]*)'
+NUM = r'(?P<NUM>\d+)'
+PLUS = r'(?P<PLUS>\+)'
+TIMES = r'(?P<TIMES>\*)'
+EQ = r'(?P<EQ>=)'
+WS = r'(?P<WS>\s+)'
+
+master_pat = re.compile('|'.join([NAME, NUM, PLUS, TIMES, EQ, WS]))
+Token = namedtuple('Token', ['type', 'value'])
+
+def generate_tokens(pat, text):
+    scanner = pat.scanner(text)
+    for m in iter(scanner.match, None):
+        yield Token(m.lastgroup, m.group())
+
+for tok in generate_tokens(master_pat, 'foo = 42'):
+    print(tok)
